fbb6eb70a9989ef3d64e226ef36cb0d9e8e480ae
weizenbock <weizenbock@users.noreply.github.com>
2017-11-12 18:35:01 +0900

    fix

--- src/mgos_arduino_sht31.h
+++ src/mgos_arduino_sht31.h
@@ -13,11 +13,11 @@ extern "C"
 #endif
 
 AE_SHT31 *mgos_sht31_create(int addr);
-void mgos_sht31_close(AE_SHT31 *sht);
-void mgos_sht31_begin(AE_SHT31 *sht);
-//int mgos_sht31_read_status(AE_SHT31 *sht31);
-int mgos_sht31_read_temperature(AE_SHT31 *sht);
-int mgos_sht31_read_humidity(AE_SHT31 *sht);
+void mgos_sht31_close(AE_SHT31 *sht31);
+void mgos_sht31_begin(AE_SHT31 *sht31);
+int mgos_sht31_read_status(AE_SHT31 *sht31);
+int mgos_sht31_read_temperature(AE_SHT31 *sht31);
+int mgos_sht31_read_humidity(AE_SHT31 *sht31);
 
 #ifdef __cplusplus
 }
